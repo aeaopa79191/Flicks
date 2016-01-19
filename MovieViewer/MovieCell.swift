a1d5c3d9681d078e1c56fb087687b7f30fb5a09d
Ka Kin Chiu <aeaopa79191@gmail.com>
2016-01-19 00:53:07 -0800

    upload

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by KaKin Chiu on 1/16/16.
+//  Copyright © 2016 KaKinChiu. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLable: UILabel!
+    
+    @IBOutlet weak var overviewLable: UILabel!
+
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
